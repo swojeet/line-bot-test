5cffede6a9447d3da88492ec451b3e149fac44bf
swojeet <swojeetkayastha@gmail.com>
2018-02-19 13:17:56 +0545

    Show response on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,39 @@
 class StaticPagesController < ApplicationController
-  def home;end
+
+  def home
+    #Redirection from Login With Line returns these
+    if params.has_key? (:code)
+      code = params[:code]
+      state = params[:state]
+      friendship_status_changed = params[:friendship_status_changed]
+      @response = get_access_token(code)
+    end
+  end
+
+  private
+  def get_access_token(code)
+    uri = URI.parse("https://api.line.me/oauth2/v2.1/token")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/x-www-form-urlencoded"
+    request.set_form_data(
+      "client_id" => "1563433382",
+      "client_secret" => "31274175a0eeb4b3f2ef4b6331fc553c
+",
+      "code" => code,
+      "grant_type" => "authorization_code",
+      "redirect_uri" => "https://linebot-staging.herokuapp.com/
+",
+    )
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    # response.code
+    response.body
+  end
 end
