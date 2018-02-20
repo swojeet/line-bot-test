fb30c982c90102ccefd6ba42b5195fb2253b60f7
swojeet <swojeetkayastha@gmail.com>
2018-02-20 14:12:50 +0545

    Update saving to database call

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,53 +11,52 @@ class StaticPagesController < ApplicationController
       @access_token = @response['access_token']
       @id_token = @response['id_token']
       @user_info = JSON.parse(UserInfoServices.get_user_info(@access_token))
-      User.find_or_create_by(line_id: @user_info['userId']) do |user|
-        user.line_id = @user_info['userId']
-        user.line_name = @user_info['displayName']
-      end
+
+      User.create(line_id: @user_info['userId'], line_name: @user_info['displayName']) unless (User.where(line_id: @user_info['userId']).exists?) 
+
     end
   end
 
-  private
-  def get_access_token(code)
-    uri = URI.parse("https://api.line.me/oauth2/v2.1/token")
-    request = Net::HTTP::Post.new(uri)
-    request.content_type = "application/x-www-form-urlencoded"
-    request.set_form_data(
-      "client_id" => "1563433382",
-      "client_secret" => "31274175a0eeb4b3f2ef4b6331fc553c",
-      "code" => code,
-      "grant_type" => "authorization_code",
-      "redirect_uri" => "https://linebot-staging.herokuapp.com/",
-    )
-
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
-    end
-
-    # response.code
-    # response.body
-    return response.body
-  end
-
-  def get_user_info(access_token)
-    uri = URI.parse("https://api.line.me/v2/profile")
-    request = Net::HTTP::Get.new(uri)
-    request["Authorization"] = "Bearer #{access_token}"
-
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
-    end
-
-    # response.code
-    response.body
-  end
+  # private
+  # def get_access_token(code)
+  #   uri = URI.parse("https://api.line.me/oauth2/v2.1/token")
+  #   request = Net::HTTP::Post.new(uri)
+  #   request.content_type = "application/x-www-form-urlencoded"
+  #   request.set_form_data(
+  #     "client_id" => "1563433382",
+  #     "client_secret" => "31274175a0eeb4b3f2ef4b6331fc553c",
+  #     "code" => code,
+  #     "grant_type" => "authorization_code",
+  #     "redirect_uri" => "https://linebot-staging.herokuapp.com/",
+  #   )
+  #
+  #   req_options = {
+  #     use_ssl: uri.scheme == "https",
+  #   }
+  #
+  #   response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  #     http.request(request)
+  #   end
+  #
+  #   # response.code
+  #   # response.body
+  #   return response.body
+  # end
+  #
+  # def get_user_info(access_token)
+  #   uri = URI.parse("https://api.line.me/v2/profile")
+  #   request = Net::HTTP::Get.new(uri)
+  #   request["Authorization"] = "Bearer #{access_token}"
+  #
+  #   req_options = {
+  #     use_ssl: uri.scheme == "https",
+  #   }
+  #
+  #   response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  #     http.request(request)
+  #   end
+  #
+  #   # response.code
+  #   response.body
+  # end
 end
