9f8a695f043b08a40c51251b92df1534caa45d31
swojeet <swojeetkayastha@gmail.com>
2018-02-21 16:01:35 +0545

    Add Send Message Button

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   post '/callback', to: 'callbacks#callback'
+  get '/send_message', to: 'callbacks#send_message'
 end
